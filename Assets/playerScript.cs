34413b585611b7a06ac2e0f408a4165c1ba3a475
dima <dima.alekseev2903@gmail.com>
2018-06-01 06:15:00 +0300

    First Commit

---
+++ Assets/playerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerScript : MonoBehaviour {
+	public float jumpForce;
+	public Rigidbody2D rb;
+	public int combo =1;
+	public int score = 0;
+	public Text uiScore;
+	public float forwardSpeed;
+	bool didFlap = false;
+
+	public bool gameOver = false;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		uiScore.text = score.ToString();
+		if (!gameOver) {
+			if (Input.GetMouseButtonDown (0))
+				didFlap = true;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		rb.AddForce (Vector2.right * forwardSpeed);
+
+		if (didFlap) {
+			rb.velocity = new Vector2 (rb.velocity.x, Vector2.up.y * jumpForce);
+			didFlap = false;
+		}
+	}
+}
